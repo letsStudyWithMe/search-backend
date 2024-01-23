768255ea3e21ed09a0364fd720b01a99a63714f1
ss <1029515726@qq.com>
2024-01-23 15:41:22 +0800

    适配器模式，注册器模式，门面模式学习

--- src/main/java/com/qi/search/facade/SearchFacade.java
+++ src/main/java/com/qi/search/facade/SearchFacade.java
@@ -1,13 +1,9 @@
-package com.qi.search.controller;
+package com.qi.search.facade;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.qi.search.common.BaseResponse;
 import com.qi.search.common.ErrorCode;
-import com.qi.search.common.ResultUtils;
+import com.qi.search.datasource.*;
 import com.qi.search.exception.BusinessException;
 import com.qi.search.exception.ThrowUtils;
-import com.qi.search.facade.SearchFacade;
-import com.qi.search.model.dto.picture.PictureQueryRequest;
 import com.qi.search.model.dto.post.PostQueryRequest;
 import com.qi.search.model.dto.search.QueryRequest;
 import com.qi.search.model.dto.user.UserQueryRequest;
@@ -15,29 +11,23 @@ import com.qi.search.model.entity.Picture;
 import com.qi.search.model.enums.SearchTypeEnum;
 import com.qi.search.model.vo.PostVO;
 import com.qi.search.model.vo.SearchVO;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qi.search.model.vo.UserVO;
 import com.qi.search.service.PictureService;
 import com.qi.search.service.PostService;
 import com.qi.search.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.CompletableFuture;
 
-/**
- *
- */
-@RestController
-@RequestMapping("/search")
+@Component
 @Slf4j
-public class SearchController {
-
+public class SearchFacade {
     @Resource
     private UserService userService;
 
@@ -48,75 +38,19 @@ public class SearchController {
     private PictureService pictureService;
 
     @Resource
-    private SearchFacade searchFacade;
+    private PostDataSource postDataSource;
 
-    @PostMapping("/all")
-    public BaseResponse<SearchVO> searchAll(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
-        return ResultUtils.success(searchFacade.searchAll(queryRequest, request));
-    }
+    @Resource
+    private UserDataSource userDataSource;
 
-    @PostMapping("/allNormal")
-    public BaseResponse<SearchVO> searchAllNormal(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
-        String searchText = queryRequest.getSearchText();
-        Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
+    @Resource
+    private PictureDataSource pictureDataSource;
 
-        UserQueryRequest userQueryRequest = new UserQueryRequest();
-        userQueryRequest.setUserName(searchText);
-        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+    @Resource
+    private DataSourceRegistry dataSourceRegistry;
 
-        PostQueryRequest postQueryRequest = new PostQueryRequest();
-        postQueryRequest.setSearchText(searchText);
-        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
 
-        SearchVO searchVO = new SearchVO();
-        searchVO.setUserList(userVOPage.getRecords());
-        searchVO.setPostList(postVOPage.getRecords());
-        searchVO.setPictureList(picturePage.getRecords());
-        return ResultUtils.success(searchVO);
-    }
-
-    @PostMapping("/allNoType")
-    public BaseResponse<SearchVO> allNoType(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
-        String searchText = queryRequest.getSearchText();
-
-        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
-            UserQueryRequest userQueryRequest = new UserQueryRequest();
-            userQueryRequest.setUserName(searchText);
-            Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-            return userVOPage;
-        });
-
-        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
-            PostQueryRequest postQueryRequest = new PostQueryRequest();
-            postQueryRequest.setSearchText(searchText);
-            Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-            return postVOPage;
-        });
-
-        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
-            Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-            return picturePage;
-        });
-
-        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
-        try {
-            Page<UserVO> userVOPage = userTask.get();
-            Page<PostVO> postVOPage = postTask.get();
-            Page<Picture> picturePage = pictureTask.get();
-
-            SearchVO searchVO = new SearchVO();
-            searchVO.setUserList(userVOPage.getRecords());
-            searchVO.setPostList(postVOPage.getRecords());
-            searchVO.setPictureList(picturePage.getRecords());
-            return ResultUtils.success(searchVO);
-        } catch (Exception e) {
-            log.error("查询异常", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
-        }
-    }
-
-    @PostMapping("/allByType")
-    public BaseResponse<SearchVO> allByType(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
+    public SearchVO searchAllOld(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
         String type = queryRequest.getType();
         SearchTypeEnum searchTypeEnum = SearchTypeEnum.getEnumByValue(type);
         ThrowUtils.throwIf(StringUtils.isBlank(type), ErrorCode.PARAMS_ERROR);
@@ -153,7 +87,7 @@ public class SearchController {
                 searchVO.setUserList(userVOPage.getRecords());
                 searchVO.setPostList(postVOPage.getRecords());
                 searchVO.setPictureList(picturePage.getRecords());
-                return ResultUtils.success(searchVO);
+                return searchVO;
             } catch (Exception e) {
                 log.error("查询异常", e);
                 throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
@@ -179,8 +113,60 @@ public class SearchController {
                     break;
                 default:
             }
-            return ResultUtils.success(searchVO);
+            return searchVO;
         }
     }
+
+    public SearchVO searchAll(@RequestBody QueryRequest queryRequest, HttpServletRequest request) {
+        String type = queryRequest.getType();
+        SearchTypeEnum searchTypeEnum = SearchTypeEnum.getEnumByValue(type);
+        ThrowUtils.throwIf(StringUtils.isBlank(type), ErrorCode.PARAMS_ERROR);
+        String searchText = queryRequest.getSearchText();
+        long current = queryRequest.getCurrent();
+        long pageSize = queryRequest.getPageSize();
+        // 搜索出所有数据
+        if (searchTypeEnum == null) {
+            CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
+                UserQueryRequest userQueryRequest = new UserQueryRequest();
+                userQueryRequest.setUserName(searchText);
+                Page<UserVO> userVOPage = userDataSource.doSearch(searchText, current, pageSize);
+                return userVOPage;
+            });
+
+            CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
+                PostQueryRequest postQueryRequest = new PostQueryRequest();
+                postQueryRequest.setSearchText(searchText);
+                Page<PostVO> postVOPage = postDataSource.doSearch(searchText, current, pageSize);
+                return postVOPage;
+            });
+
+            CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
+                Page<Picture> picturePage = pictureDataSource.doSearch(searchText, 1, 10);
+                return picturePage;
+            });
+
+            CompletableFuture.allOf(userTask, postTask, pictureTask).join();
+            try {
+                Page<UserVO> userVOPage = userTask.get();
+                Page<PostVO> postVOPage = postTask.get();
+                Page<Picture> picturePage = pictureTask.get();
+                SearchVO searchVO = new SearchVO();
+                searchVO.setUserList(userVOPage.getRecords());
+                searchVO.setPostList(postVOPage.getRecords());
+                searchVO.setPictureList(picturePage.getRecords());
+                return searchVO;
+            } catch (Exception e) {
+                log.error("查询异常", e);
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
+            }
+        } else {
+            SearchVO searchVO = new SearchVO();
+            DataSource<?> dataSource = dataSourceRegistry.getDataSourceByType(type);
+            Page<?> page = dataSource.doSearch(searchText, current, pageSize);
+            searchVO.setDataList(page.getRecords());
+            return searchVO;
+        }
+    }
+
 
 }
--- src/main/java/com/qi/search/model/vo/SearchVO.java
+++ src/main/java/com/qi/search/model/vo/SearchVO.java
@@ -8,6 +8,13 @@ import java.util.List;
 @Data
 public class SearchVO {
     private List<UserVO> userList;
+
     private List<PostVO> postList;
+
     private List<Picture> pictureList;
+
+    private List<?> dataList;
+
+    private static final long serialVersionUID = 1L;
+
 }
